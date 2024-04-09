3e37884a80e2f2ee9d079d9063d02f4bf0e80bce
RochelleHere <114105211+RochelleHere@users.noreply.github.com>
2024-04-09 11:54:10 +0100

    Add files via upload

---
+++ box.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent("box",{
+    schema:{
+        movex:{type:"number", default:1}
+    },
+    tick:function(){
+        this.data.movex -= 0.01
+        var pos=this.el.getAttribute("position")
+        pos.x=this.data.movex
+        this.el.setAttribute("position",{x:pos.x, y:pos.y, z:pos.z})
+    }
+})
